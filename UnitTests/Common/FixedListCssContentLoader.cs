ca8c1686320272c6cdb41fae21c2aba440f97f1e
ProductiveRage <742348+ProductiveRage@users.noreply.github.com>
2012-02-09 19:05:47 +0000

    Added UnitTests and included fix for comment removal that deals with unclosed comments

---
+++ UnitTests/Common/FixedListCssContentLoader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CSSMinifier.FileLoaders;
+
+namespace UnitTests.Common
+{
+	public class FixedListCssContentLoader : ITextFileLoader
+	{
+		private List<TextFileContents> _files;
+		public FixedListCssContentLoader(params TextFileContents[] files)
+		{
+			if (files == null)
+				throw new ArgumentNullException("files");
+			var filesTidied = new List<TextFileContents>();
+			foreach (var file in files)
+			{
+				if (file == null)
+					throw new ArgumentException("Null value encountered in files");
+				filesTidied.Add(file);
+			}
+			_files = filesTidied;
+		}
+
+		public TextFileContents Load(string filename)
+		{
+			if (string.IsNullOrWhiteSpace(filename))
+				throw new ArgumentException("Null/blank filename specified");
+			var file = _files.FirstOrDefault(f => f.Filename.Replace("/", "\\").Equals(filename.Replace("/", "\\"), StringComparison.InvariantCultureIgnoreCase));
+			if (file == null)
+				throw new ArgumentException("Unsupported filename: " + filename);
+			return file;
+		}
+	}
+}
---
+++ UnitTests/MinifyingCssLoaderTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using CSSMinifier.FileLoaders;
+using UnitTests.Common;
+using Xunit;
+
+namespace UnitTests
+{
+	public class MinifyingCssLoaderTests
+	{
+		[Fact]
+		public void EnsureUnclosedCommentsAreRemoved()
+		{
+			var content = "/* Test 1 */\r\np { color: blue; }\r\n/*\r\n";
+			var expected = "p{color:blue}";
+			
+			var contentLoader = new MinifyingCssLoader(
+				new FixedListCssContentLoader(
+					new TextFileContents("Test.css", new DateTime(2011, 11, 26, 14, 07, 29), content)
+				)
+			);
+			Assert.Equal(expected, contentLoader.Load("Test.css").Content);
+		}
+
+		[Fact]
+		public void EnsureSquareBracketsAreSupported()
+		{
+			var content = "h2#Header a[title~=\"value\"] { font-size: 120%; }\r\n";
+			var expected = "h2#Header a[title~=\"value\"]{font-size:120%}";
+
+			var contentLoader = new MinifyingCssLoader(
+				new FixedListCssContentLoader(
+					new TextFileContents("Test.css", new DateTime(2011, 11, 26, 14, 07, 29), content)
+				)
+			);
+			Assert.Equal(expected, contentLoader.Load("Test.css").Content);
+		}
+
+		[Fact]
+		public void EnsureHyphenatedPseudoClassesAreSupported()
+		{
+			var content = "p.Highlight a:last-child { font-weight: bold; }\r\n";
+			var expected = "p.Highlight a:last-child{font-weight:bold}";
+
+			var contentLoader = new MinifyingCssLoader(
+				new FixedListCssContentLoader(
+					new TextFileContents("Test.css", new DateTime(2011, 11, 26, 14, 07, 29), content)
+				)
+			);
+			Assert.Equal(expected, contentLoader.Load("Test.css").Content);
+		}
+	}
+}
