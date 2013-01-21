5b29c1cd5cb9266f2d8584f4cd53d0eef76e78f1
dan <dan@newmind.co.uk>
2013-01-21 19:49:30 +0000

    DotLessCssCssLoader allows for an ILogEvents reference to be passed in to handle events logged by the LessEngine, instead of just swallowing them

--- CSSMinifier/FileLoaders/DotLessCssCssLoader.cs
+++ CSSMinifier/FileLoaders/DotLessCssCssLoader.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CSSMinifier.Logging;
 using dotless.Core;
 using dotless.Core.Loggers;
 using dotless.Core.Parser;
@@ -9,15 +10,19 @@ namespace CSSMinifier.FileLoaders
 	{
 		private ITextFileLoader _contentLoader;
 		private LessCssMinificationTypeOptions _minificationType;
-		public DotLessCssCssLoader(ITextFileLoader contentLoader, LessCssMinificationTypeOptions minificationType)
+		private ILogEvents _logger;
+		public DotLessCssCssLoader(ITextFileLoader contentLoader, LessCssMinificationTypeOptions minificationType, ILogEvents logger)
 		{
 			if (contentLoader == null)
 				throw new ArgumentNullException("contentLoader");
 			if (!Enum.IsDefined(typeof(LessCssMinificationTypeOptions), minificationType))
 				throw new ArgumentOutOfRangeException("minificationType");
+			if (logger == null)
+				throw new ArgumentNullException("logger");
 
 			_contentLoader = contentLoader;
 			_minificationType = minificationType;
+			_logger = logger;
 		}
 
 		public enum LessCssMinificationTypeOptions
@@ -37,7 +42,7 @@ namespace CSSMinifier.FileLoaders
 			var initialFileContents = _contentLoader.Load(relativePath);
 			var engine = new LessEngine(
 				new Parser(),
-				new NullDotLessCssLogger(),
+				new DotLessCssPassThroughLogger(_logger),
 				_minificationType == LessCssMinificationTypeOptions.Minify
 			);
 			return new TextFileContents(
@@ -47,13 +52,77 @@ namespace CSSMinifier.FileLoaders
 			);
 		}
 
-		private class NullDotLessCssLogger : ILogger
+		private class DotLessCssPassThroughLogger : ILogger
 		{
-			public void Debug(string message) { }
-			public void Error(string message) { }
-			public void Info(string message) { }
-			public void Log(LogLevel level, string message) { }
-			public void Warn(string message) { }
+			private ILogEvents _logger;
+			public DotLessCssPassThroughLogger(ILogEvents logger)
+			{
+				if (logger == null)
+					throw new ArgumentNullException("logger");
+
+				_logger = logger;
+			}
+
+			public void Debug(string message)
+			{
+				if (string.IsNullOrWhiteSpace(message))
+					return;
+
+				_logger.LogIgnoringAnyError(Logging.LogLevel.Debug, () => message);
+			}
+
+			public void Error(string message)
+			{
+				if (string.IsNullOrWhiteSpace(message))
+					return;
+
+				_logger.LogIgnoringAnyError(Logging.LogLevel.Debug, () => message);
+			}
+
+			public void Info(string message)
+			{
+				if (string.IsNullOrWhiteSpace(message))
+					return;
+
+				_logger.LogIgnoringAnyError(Logging.LogLevel.Debug, () => message);
+			}
+
+			public void Log(dotless.Core.Loggers.LogLevel level, string message)
+			{
+				if (string.IsNullOrWhiteSpace(message))
+					return;
+
+				switch (level)
+				{
+					case dotless.Core.Loggers.LogLevel.Debug:
+						Debug(message);
+						return;
+
+					case dotless.Core.Loggers.LogLevel.Error:
+						Error(message);
+						return;
+
+					case dotless.Core.Loggers.LogLevel.Info:
+						Info(message);
+						return;
+
+					case dotless.Core.Loggers.LogLevel.Warn:
+						Warn(message);
+						return;
+
+					default:
+						_logger.LogIgnoringAnyError(Logging.LogLevel.Warning, () => "DotLess logged message with unsupported LogLeve [" + level.ToString() + "]: " + message);
+						return;
+				}
+			}
+
+			public void Warn(string message)
+			{
+				if (string.IsNullOrWhiteSpace(message))
+					return;
+
+				_logger.LogIgnoringAnyError(Logging.LogLevel.Debug, () => message);
+			}
 		}
 	}
 }
