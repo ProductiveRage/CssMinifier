395f6897857d5b7a84b9c7018aeaa38438302527
dan <dan@newmind.co.uk>
2013-01-28 23:05:32 +0000

    Added LessCssLineNumberingTextFileLoader and InjectedIdTidyingTextFileLoader which allow pseudo source mapping for locating style declarations in original source files (by filename and line number) by injecting Html-Id-esque markers into the content

---
+++ CSSMinifier/FileLoaders/InjectedIdTidyingTextFileLoader.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSSMinifier.FileLoaders
+{
+	/// <summary>
+	/// This is intended as a complement to the LessCssLineNumberingStyleSheetProcessor when used with LESS-supported nested style selectors - in this scenario, too many
+	/// line number markers will be present in the final content -
+	///   eg.
+	///     div.Wrapper {
+	///       h2 {
+	///         font-weight: bold;
+	///       }
+	///     }
+	///   may become (with marker insertion)
+	///     #Test1.css_1, div.Wrapper {
+	///       #Test1.css_2, h2 {
+	///         font-weight: bold;
+	///       }
+	///     }
+	///   which is translated (by the dotLess Compiler into)
+	///     #Test1.css_1 #Test1.css_2, div.Wrapper #Test1.css_2, #Test1.css_1 h2, div.Wrapper h2 { font-weight: bold; }
+	///   of which only the last two selectors are desirable (the third selector because it indicates the location of the style and the fourth selector since it's the
+	///   actual style which was declared in the first place!).
+	///   
+	/// This will take the list of HtmlId-esque marker insertions (of the form "#id,") that have been inserted into the content and will remove selectors that are
+	/// generated that are not of benefit to the final content (the first and second selectors in the translated content in the example above).
+	/// </summary>
+	public class InjectedIdTidyingTextFileLoader : ITextFileLoader
+	{
+		private readonly ITextFileLoader _fileLoader;
+		private readonly InsertedMarkerRetriever _insertedMarkerRetriever;
+		public InjectedIdTidyingTextFileLoader(ITextFileLoader fileLoader, InsertedMarkerRetriever insertedMarkerRetriever)
+		{
+			if (fileLoader == null)
+				throw new ArgumentNullException("fileLoader");
+			if (insertedMarkerRetriever == null)
+				throw new ArgumentNullException("insertedMarkerRetriever");
+
+			_fileLoader = fileLoader;
+			_insertedMarkerRetriever = insertedMarkerRetriever;
+		}
+
+		/// <summary>
+		/// This may never return null, nor a set containing any null or blank entries. All markers must be of the format "#id," (leading or trailing whitespace is allowed
+		/// and ignored). An exception will be raised if these conditions are not met.
+		/// </summary>
+		public delegate IEnumerable<string> InsertedMarkerRetriever();
+
+		/// <summary>
+		/// This will never return null, it will throw an exception for a null or empty filename - it is up to the particular implementation whether or not to throw an
+		/// exception for invalid / inaccessible filenames (if no exception is thrown, the issue should be logged). It is up the the implementation to handle mapping
+		/// the relative path to a full file path.
+		/// </summary>
+		public TextFileContents Load(string relativePath)
+		{
+			if (string.IsNullOrWhiteSpace(relativePath))
+				throw new ArgumentException("Null/blank relativePath specified");
+
+			var unprocessedContent = _fileLoader.Load(relativePath);
+			return new TextFileContents(
+				unprocessedContent.RelativePath,
+				unprocessedContent.LastModified,
+				Process(unprocessedContent.Content)
+			);
+		}
+
+		private string Process(string content)
+		{
+			if (content == null)
+				throw new ArgumentNullException("content");
+
+			// If there's no content to process then return it unaltered
+			if (content.Trim() == "")
+				return content;
+
+			// Retrieve the inserted Ids that may need tidying up (if none were inserted then return the content unaltered)
+			var insertedIds = GetInsertedIds();
+			if (!insertedIds.Any())
+				return content;
+
+			var stringBuilder = new StringBuilder();
+			var bufferBuilder = new StringBuilder();
+			foreach (var cssSegment in CSSParser.Parser.ParseLESS(content))
+			{
+				// Ignore any comment content as it will only interfere with the processing we want to do (and we expect it to have been removed
+				// by the time we get to this processing step anyway)
+				if (cssSegment.CharacterCategorisation == CSSParser.ContentProcessors.CharacterCategorisationOptions.Comment)
+					continue;
+
+				// If we encounter whitespace and we're not part way through some "selector-or-style-property" content then just push it to the
+				// output and move on
+				if ((cssSegment.CharacterCategorisation == CSSParser.ContentProcessors.CharacterCategorisationOptions.Whitespace) && (bufferBuilder.Length == 0))
+				{
+					stringBuilder.Append(cssSegment.Value);
+					continue;
+				}
+
+				// If we encounter SelectorOrStyleProperty content or Whitespace when we're already part-way through SelectorOrStyleProperty
+				// content, then push the content to the buffer to be dealt with when we get to the end of the content
+				if ((cssSegment.CharacterCategorisation == CSSParser.ContentProcessors.CharacterCategorisationOptions.SelectorOrStyleProperty)
+				|| (cssSegment.CharacterCategorisation == CSSParser.ContentProcessors.CharacterCategorisationOptions.Whitespace))
+				{
+					bufferBuilder.Append(cssSegment.Value);
+					continue;
+				}
+
+				// This indicates that the "selector-or-style-property" content (if we're in the middle of processing any) was a style property,
+				// which we don't have to process
+				if (cssSegment.CharacterCategorisation == CSSParser.ContentProcessors.CharacterCategorisationOptions.StylePropertyColon)
+				{
+					if (bufferBuilder.Length > 0)
+					{
+						stringBuilder.Append(bufferBuilder.ToString());
+						bufferBuilder.Clear();
+					}
+					stringBuilder.Append(cssSegment.Value);
+					continue;
+				}
+
+				// Any other content type indicates the end of "selector-or-style-property" (if we have any in the buffer), so it needs processing
+				if (bufferBuilder.Length > 0)
+				{
+					stringBuilder.Append(
+						TidySelectorContent(bufferBuilder.ToString(), insertedIds)
+					);
+					bufferBuilder.Clear();
+				}
+				stringBuilder.Append(cssSegment.Value);
+			}
+			if (bufferBuilder.Length > 0)
+			{
+				stringBuilder.Append(
+					TidySelectorContent(bufferBuilder.ToString(), insertedIds)
+				);
+			}
+			return stringBuilder.ToString();
+		}
+
+		/// <summary>
+		/// Given a CSS selector (that may be a comma-separated string of multiple selectors), it will remove any selector content that relates to injected
+		/// marker ids other than the most specific selector for that id. (Where ids are injected into nested LESS style declarations, less specific ids
+		/// from earlier line numbers may appear with more specific ids for the nested properties - we only want the injected id that is closest to the
+		/// style property, since that is the most descriptive).
+		/// </summary>
+		private string TidySelectorContent(string cssSelector, IEnumerable<HtmlId> insertedIds)
+		{
+			if (cssSelector == null)
+				throw new ArgumentNullException("cssSelector");
+			if (insertedIds == null)
+				throw new ArgumentNullException("insertedIds");
+
+			var insertedIdsArray = insertedIds.ToArray();
+			if (insertedIdsArray.Any(id => id == null))
+				throw new ArgumentException("Null reference encountered in insertedIds set");
+
+			// Maintain if selector contains no inserted markers (eg. section div.Whatever h2)
+			//    eg. "section div.Whatever h2"
+			// - This will always be the case with the last entry since markers are always inserted into the start of selector chains
+
+			// Maintain if the penultimate selector segment is the ONLY inserted marker
+			//    eg. "#Test19.css_1 div.Whatever"
+			//    eg. "section #Test19.css_10 h2"
+
+			// Selectors we don't want may be of the form
+			//    "#Test19.css_1 #Test19.css_10 h2"
+
+			var tidiedSelectors = new List<string>();
+			foreach (var selector in cssSelector.Split(',').Select(s => s.Trim()).Where(s => s != ""))
+			{
+				// Passign a null char[] to Split will break the string on all whitespace
+				var selectorSegments = selector.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+				if (selectorSegments.Length == 0)
+					continue;
+
+				var shouldSelectorBeMaintained = true;
+				for (var index = 0; index < selectorSegments.Length; index++)
+				{
+					// The penultimate segment (for cases where there are more than one segment) may be an injected id but none of the others may be
+					// (if it's not an injected id, that doesn't matter - we allow it either way). If there is only one segment then this condition
+					// will never be entered.
+					if (index == (selectorSegments.Length - 2))
+						continue;
+
+					var selectorSegment = selectorSegments[index];
+					if (insertedIds.Any(id => selectorSegment == ("#" + id.Value)))
+					{
+						shouldSelectorBeMaintained = false;
+						break;
+					}
+				}
+				if (shouldSelectorBeMaintained)
+					tidiedSelectors.Add(selector);
+			}
+			return string.Join(",", tidiedSelectors);
+		}
+
+		/// <summary>
+		/// This will never return null, nor a set containing any nulls. It may return an empty set if no Ids have been reported as being inserted into the content.
+		/// </summary>
+		private IEnumerable<HtmlId> GetInsertedIds()
+		{
+			var insertedMarkers = _insertedMarkerRetriever();
+			if (insertedMarkers == null)
+				throw new Exception("InsertedMarkerRetriever returned null reference");
+
+			var insertedMarkersArray = insertedMarkers.ToArray();
+			if (insertedMarkersArray.Any(m => m == null))
+				throw new Exception("InsertedMarkerRetriever returned a set containing a null reference");
+			var ids = new List<HtmlId>();
+			for (var index = 0; index < insertedMarkersArray.Length; index++)
+			{
+				var marker = insertedMarkersArray[index].Trim();
+				if (!marker.StartsWith("#") || !marker.EndsWith(","))
+					throw new Exception("InsertedMarkerRetriever returned a set containing an invalid value: " + marker);
+
+				try
+				{
+					ids.Add(new HtmlId(marker.Substring(1, marker.Length - 2)));
+				}
+				catch (ArgumentException e)
+				{
+					throw new Exception("InsertedMarkerRetriever returned a set containing an invalid value: " + marker, e);
+				}
+			}
+			return ids;
+		}
+
+		private class HtmlId
+		{
+			public HtmlId(string value)
+			{
+				if (string.IsNullOrWhiteSpace(value))
+					throw new ArgumentNullException("Null/blank value specified");
+
+				// Not a lot of point going to town over the validation here, ensuring it's not blank, doesn't contain any whitespace part-way through and
+				// doesn't have a hash symbol in it should do the job fine
+				value = value.Trim();
+				if (value.Any(c => char.IsWhiteSpace(c)))
+					throw new ArgumentException("Id values may not contain whitespace (other than leading and/or trailing whitespace, which will be stripped");
+				if (value.Contains('#'))
+					throw new ArgumentException("Id values may not contain the hash character");
+
+				Value = value;
+			}
+
+			/// <summary>
+			/// This will never be null or blank
+			/// </summary>
+			public string Value { get; private set; }
+		}
+	}
+}
---
+++ CSSMinifier/FileLoaders/LessCssLineNumberingTextFileLoader.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace CSSMinifier.FileLoaders
+{
+	/// <summary>
+	/// This will insert markers into style declaration headers that may indicate which line number the declaration header ended on. If a marker format is specified that injects a new
+	/// html id then minified / combined style declarations can be mapped back to their source file and location (in this case the marker format would specify a fixed filename per
+	/// file and include the line number). The markerFormat will have the string "{0}" replaced with the relativePath (altered to try to generate html-id-friendly strings) and
+	/// "{1}" with the appropriate line number. In the case of LessCss nested style declarations, only the top-level declaration header will be considered elligible for marker
+	/// insertion. This will work best on content that has has comments removed since there is no provision in this code to ignore comments (if comments are not removed then
+	/// feasibly some markers will not be inserted correctly or at all).
+	/// WARNING: This will not insert markers if the declaration header is wrapped in a media query. If LessCSS is used then media queries may be nested inside declarations,
+	/// this WILL still insert markers as expected.
+	/// </summary>
+	public class LessCssLineNumberingTextFileLoader : ITextFileLoader
+	{
+		private readonly ITextFileLoader _fileLoader;
+		private readonly MarkerGenerator _markerGenerator;
+		public LessCssLineNumberingTextFileLoader(ITextFileLoader fileLoader, MarkerGenerator markerGenerator)
+		{
+			if (fileLoader == null)
+				throw new ArgumentNullException("fileLoader");
+			if (markerGenerator == null)
+				throw new ArgumentNullException("markerGenerator");
+
+			_fileLoader = fileLoader;
+			_markerGenerator = markerGenerator;
+		}
+
+		/// <summary>
+		/// This will never be called with a null or blank relativePath nor a lineNumber of zero or less. It may return null or blank, meaning no insertion will be made.
+		/// The returned content (if any) is not escaped so may alter the markup if required.
+		/// </summary>
+		public delegate string MarkerGenerator(string relativePath, int lineNumber);
+
+		/// <summary>
+		/// This will never return null, it will throw an exception for a null or empty filename - it is up to the particular implementation whether or not to throw an
+		/// exception for invalid / inaccessible filenames (if no exception is thrown, the issue should be logged). It is up the the implementation to handle mapping
+		/// the relative path to a full file path.
+		/// </summary>
+		public TextFileContents Load(string relativePath)
+		{
+			if (string.IsNullOrWhiteSpace(relativePath))
+				throw new ArgumentException("Null/blank relativePath specified");
+
+			var unprocessedContent = _fileLoader.Load(relativePath);
+			return new TextFileContents(
+				unprocessedContent.RelativePath,
+				unprocessedContent.LastModified,
+				Process(unprocessedContent.Content, relativePath)
+			);
+		}
+
+		/// <summary>
+		/// This will throw an exception for null content or if otherwise unable to satisfy the request, it will never return null
+		/// </summary>
+		private string Process(string content, string relativePath)
+		{
+			if (content == null)
+				throw new ArgumentNullException("content");
+			if (string.IsNullOrWhiteSpace(relativePath))
+				throw new ArgumentException("Null/blank relativePath specified");
+
+			// Standardlise line breaks to newline characters only
+			content = content.Replace("\r\n", "\n").Replace('\r', '\n');
+
+			// We're going to process backwards through the data so we need the total number of lines there are so that we know the number line we're starting on
+			var lineNumber = content.Split('\n').Length;
+			IAnalyseCharacters contentAnalyser = new StandardContentAnalyser();
+			var stringBuilder = new StringBuilder();
+			for (var index = content.Length - 1; index >= 0; index--)
+			{
+				var currentCharacter = content[index];
+				var analysisResult = contentAnalyser.Analyse(
+					currentCharacter,
+					(index > 0) ? (char?)content[index - 1] : null
+				);
+				if (analysisResult.MarkerInsertionType == MarkerInsertionTypeOptions.InsertAfterCurrentCharacter)
+					stringBuilder.Insert(0, _markerGenerator(relativePath, lineNumber + analysisResult.MarkerLineNumberOffset) ?? "");
+				stringBuilder.Insert(0, currentCharacter);
+				if (analysisResult.MarkerInsertionType == MarkerInsertionTypeOptions.InsertBeforeCurrentCharacter)
+					stringBuilder.Insert(0, _markerGenerator(relativePath, lineNumber + analysisResult.MarkerLineNumberOffset) ?? "");
+				contentAnalyser = analysisResult.NextAnalyser;
+
+				if (currentCharacter == '\n')
+					lineNumber--;
+			}
+			return stringBuilder.ToString();
+		}
+
+		private interface IAnalyseCharacters
+		{
+			AnalysisResult Analyse(char currentCharacter, char? previousCharacter);
+		}
+
+		private class AnalysisResult
+		{
+			public static AnalysisResult InsertAfterCurrentCharacter(int markerLineNumberOffset, IAnalyseCharacters nextProcessor)
+			{
+				return new AnalysisResult(MarkerInsertionTypeOptions.InsertAfterCurrentCharacter, markerLineNumberOffset, nextProcessor);
+			}
+			public static AnalysisResult InsertBeforeCurrentCharacter(int markerLineNumberOffset, IAnalyseCharacters nextProcessor)
+			{
+				return new AnalysisResult(MarkerInsertionTypeOptions.InsertBeforeCurrentCharacter, markerLineNumberOffset, nextProcessor);
+			}
+			public static AnalysisResult NoInsertion(IAnalyseCharacters nextProcessor)
+			{
+				return new AnalysisResult(MarkerInsertionTypeOptions.NoInsertion, 0, nextProcessor);
+			}
+
+			private AnalysisResult(MarkerInsertionTypeOptions markerInsertionType, int markerLineNumberOffset, IAnalyseCharacters nextProcessor)
+			{
+				if (!Enum.IsDefined(typeof(MarkerInsertionTypeOptions), markerInsertionType))
+					throw new ArgumentOutOfRangeException("markerInsertionType");
+				if (markerLineNumberOffset < 0)
+					throw new ArgumentOutOfRangeException("markerLineNumberOffset", "must be zero or greater");
+				if (nextProcessor == null)
+					throw new ArgumentNullException("nextProcessor");
+
+				MarkerInsertionType = markerInsertionType;
+				MarkerLineNumberOffset = markerLineNumberOffset;
+				NextAnalyser = nextProcessor;
+			}
+
+			public MarkerInsertionTypeOptions MarkerInsertionType { get; private set; }
+
+			public int MarkerLineNumberOffset { get; private set; }
+
+			/// <summary>
+			/// This will never be null
+			/// </summary>
+			public IAnalyseCharacters NextAnalyser { get; private set; }
+		}
+
+		private enum MarkerInsertionTypeOptions
+		{
+			InsertAfterCurrentCharacter,
+			InsertBeforeCurrentCharacter,
+			NoInsertion
+		}
+
+		private class StandardContentAnalyser : IAnalyseCharacters
+		{
+			public AnalysisResult Analyse(char currentCharacter, char? previousCharacter)
+			{
+				if (currentCharacter == '{')
+					return AnalysisResult.NoInsertion(new DeclarationHeaderAnalyser());
+
+				return AnalysisResult.NoInsertion(this);
+			}
+		}
+
+		private class DeclarationHeaderAnalyser : IAnalyseCharacters
+		{
+			// What about media queries???? The latter is supported, but not the former
+			//
+			// @media (width: 400px) {
+			//   .test {
+			//     color: red;
+			//   }
+			// }
+			//
+			// or
+			//
+			// .test {
+			//   @media (width: 400px) {
+			//     color: red;
+			//   }
+			// }
+
+			private static char[] _declarationHeaderTerminators_MarkerInserting = new[]
+			{
+				'}', // Expect this to be the end of another style block, the current style declaration header has terminated
+				';'  // This could be the end of a LessCSS variable declaration, the current style declaration header has terminated
+			};
+			private static char[] _declarationHeaderTerminators_NonMarkerInserting = new[]
+			{
+				')'  // Indicates that the declaration header is a LessCSS parameterised mixin, we don't want mark these
+			};
+			private static char[] _declarationHeaderTerminators_Reset = new[]
+			{
+				'{'  // Presumably part of a nested LessCSS style block, reset since we weren't in the top-level declaration header
+			};
+
+			private readonly int _declarationHeaderLineNumberOffset;
+			private readonly bool _encounteredNonWhiteSpaceDeclarationContent;
+			public DeclarationHeaderAnalyser() : this(0, false) { }
+			private DeclarationHeaderAnalyser(int declarationHeaderLineNumberOffset, bool encounteredNonWhiteSpaceDeclarationContent)
+			{
+				if (declarationHeaderLineNumberOffset < 0)
+					throw new ArgumentOutOfRangeException("declarationHeaderLineNumberOffset", "must be zero or greater");
+
+				_declarationHeaderLineNumberOffset = declarationHeaderLineNumberOffset;
+				_encounteredNonWhiteSpaceDeclarationContent = encounteredNonWhiteSpaceDeclarationContent;
+			}
+
+			public AnalysisResult Analyse(char currentCharacter, char? previousCharacter)
+			{
+				// If there is no more content to process then this must be a marker insertion point since we're in a declaration header
+				if (previousCharacter == null)
+					return AnalysisResult.InsertBeforeCurrentCharacter(_declarationHeaderLineNumberOffset, new StandardContentAnalyser());
+
+				if (_declarationHeaderTerminators_MarkerInserting.Contains(currentCharacter))
+					return AnalysisResult.InsertAfterCurrentCharacter(_declarationHeaderLineNumberOffset, new StandardContentAnalyser());
+				else if (_declarationHeaderTerminators_NonMarkerInserting.Contains(currentCharacter))
+					return AnalysisResult.NoInsertion(new StandardContentAnalyser());
+				else if (_declarationHeaderTerminators_Reset.Contains(currentCharacter))
+					return AnalysisResult.NoInsertion(new DeclarationHeaderAnalyser());
+
+				// If we've encountered a line break then we'll need to add one the marker line number offset - if there's loads of new lines between a declaration header
+				// and the preceding content, the line number indicated in the marker should point to a line that includes part of the declaration header rather than
+				// being the end of the new line / whitespace content from the content before it. This offset is only incremented if some non-whitespace content has
+				// been encountered in the declaration header, so we indicate a class name rather than open bracket (when they're not on the same line), for example
+				if ((currentCharacter == '\n') && _encounteredNonWhiteSpaceDeclarationContent)
+					return AnalysisResult.NoInsertion(new DeclarationHeaderAnalyser(_declarationHeaderLineNumberOffset + 1, true));
+
+				return AnalysisResult.NoInsertion(
+					new DeclarationHeaderAnalyser(
+						_declarationHeaderLineNumberOffset,
+						_encounteredNonWhiteSpaceDeclarationContent || !char.IsWhiteSpace(currentCharacter)
+					)
+				);
+			}
+		}
+	}
+}
