a570856aa8591f464d5119919ae5579c33e6274e
dan <dan@newmind.co.uk>
2013-01-31 22:12:07 +0000

    Added LessCssOpeningBodyTagRenamer and ContentReplacingTextFileLoader classes which allow individual files to be wrapped in a "body { }" tag to restrict the scope of any LESS values and mixins, but then for the extra "body" tag to be removed from the selectors in the compiled CSS

---
+++ CSSMinifier/FileLoaders/ContentReplacingTextFileLoader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CSSMinifier.FileLoaders
+{
+	/// <summary>
+	/// This will perform a simple string replacement on the loaded content. It is intended for use with LessCssOpeningBodyTagRenamer (see its comments for more details)
+	/// </summary>
+	public class ContentReplacingTextFileLoader : ITextFileLoader
+	{
+		private readonly ITextFileLoader _fileLoader;
+		private readonly string _stringToBeReplaced;
+		private readonly string _stringToReplaceWith;
+		public ContentReplacingTextFileLoader(ITextFileLoader fileLoader, string stringToBeReplaced, string stringToReplaceWith)
+		{
+			if (fileLoader == null)
+				throw new ArgumentNullException("fileLoader");
+			if (string.IsNullOrEmpty(stringToBeReplaced))
+				throw new ArgumentException("Null/blank stringToBeReplaced specified");
+			if (stringToReplaceWith == null)
+				throw new ArgumentNullException("stringToReplaceWith");
+
+			_fileLoader = fileLoader;
+			_stringToBeReplaced = stringToBeReplaced;
+			_stringToReplaceWith = stringToReplaceWith;
+		}
+
+		/// <summary>
+		/// This will never return null, it will throw an exception for a null or empty filename - it is up to the particular implementation whether or not to throw an
+		/// exception for invalid / inaccessible filenames (if no exception is thrown, the issue should be logged). It is up the the implementation to handle mapping
+		/// the relative path to a full file path.
+		/// </summary>
+		public TextFileContents Load(string relativePath)
+		{
+			if (string.IsNullOrWhiteSpace(relativePath))
+				throw new ArgumentException("Null/blank relativePath specified");
+
+			var unprocessedContent = _fileLoader.Load(relativePath);
+			return new TextFileContents(
+				unprocessedContent.RelativePath,
+				unprocessedContent.LastModified,
+				unprocessedContent.Content.Replace(_stringToBeReplaced, _stringToReplaceWith)
+			);
+		}
+	}
+}
