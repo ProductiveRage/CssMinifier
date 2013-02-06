0819920dc4f0cec4f4345a701eaaae2f57bdfac2
Dan Roberts <dan@newmind.co.uk>
2013-02-06 17:59:08 +0000

    Altered SingleFolderLastModifiedDateRetriever to enable file extensions to optionally be specified (if not then ALL files will be considered) so that the CSSFileDiskCache could write to the same folder as the source files so long as it doesn't write files with a "css" or "less" extension

--- CSSMinifier/LastModifiedDateRetrievers/SingleFolderLastModifiedDateRetriever.cs
+++ CSSMinifier/LastModifiedDateRetrievers/SingleFolderLastModifiedDateRetriever.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using CSSMinifier.PathMapping;
@@ -11,14 +12,26 @@ namespace CSSMinifier.FileLoaders.LastModifiedDateRetrievers
 	/// </summary>
 	public class SingleFolderLastModifiedDateRetriever : ILastModifiedDateRetriever
 	{
-		private IRelativePathMapper _relativePathMapper;
-		public SingleFolderLastModifiedDateRetriever(IRelativePathMapper relativePathMapper)
+		private readonly IRelativePathMapper _relativePathMapper;
+		private readonly string[] _extensionRestrictions;
+		public SingleFolderLastModifiedDateRetriever(IRelativePathMapper relativePathMapper, IEnumerable<string> optionalExtensionRestrictions)
 		{
 			if (relativePathMapper == null)
 				throw new ArgumentNullException("relativePathMapper");
 
+			if (optionalExtensionRestrictions == null)
+				_extensionRestrictions = new[] { "*" };
+			else
+			{
+				var optionalExtensionRestrictionsTidied = optionalExtensionRestrictions.ToList();
+				if (optionalExtensionRestrictionsTidied.Any(e => string.IsNullOrWhiteSpace(e)))
+					throw new ArgumentException("Encountered null or blank entry in optionalExtensionRestrictions set");
+				_extensionRestrictions = optionalExtensionRestrictionsTidied.Select(e => e.Trim().ToLower()).Distinct().ToArray();
+			}
+
 			_relativePathMapper = relativePathMapper;
 		}
+		public SingleFolderLastModifiedDateRetriever(IRelativePathMapper relativePathMapper) : this(relativePathMapper, null) { }
 
 		/// <summary>
 		/// This will raise an exception if unable to determine the last modified date or if a null or empty relativePath is specified
@@ -33,7 +46,10 @@ namespace CSSMinifier.FileLoaders.LastModifiedDateRetrievers
 			);
 			if (!file.Exists)
 				throw new ArgumentException("Invalid relativePath - file does not exist: " + relativePath);
-			return file.Directory.GetFiles().Max(f => f.LastWriteTime);
+
+			return _extensionRestrictions
+				.SelectMany(extension => file.Directory.GetFiles("*." + extension))
+				.Max(f => f.LastWriteTime);
 		}
 	}
 }
