7e114fbbfa3a5fa0454e475070af05e21d693d68
Dan Roberts <742348+ProductiveRage@users.noreply.github.com>
2013-02-06 17:58:15 +0000

    Added CSSFileDiskCache which will allow for compiled content to be stored on disk instead of having to be in memory (or to work as well as memory, as a more persistent store)

---
+++ UnitTests/CSSFileDiskCacheTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CSSMinifier.Caching;
+using CSSMinifier.FileLoaders;
+using Xunit;
+
+namespace UnitTests
+{
+	public class CSSFileDiskCacheTests
+	{
+		/// <summary>
+		/// This only tests the most basic case, edge cases will need additional tests
+		/// </summary>
+		[Fact]
+		public void StraightDownTheRoadCopyShouldSucceed()
+		{
+			var source = new TextFileContents(
+				"styles.css",
+				new DateTime(2013, 2, 6, 18, 19, 0),
+				".Woo{color:black}"
+			);
+
+			var contentRepresentation = CSSFileDiskCache.GetFileContentRepresentation(source);
+			
+			TextFileContents copy;
+			using (var reader = new StringReader(contentRepresentation))
+			{
+				copy = CSSFileDiskCache.GetFileContents(reader, source.LastModified);
+			}
+
+			Assert.Equal<TextFileContents>(
+				source,
+				copy,
+				new TextFileContentsComparer()
+			);
+		}
+
+		private class TextFileContentsComparer : IEqualityComparer<TextFileContents>
+		{
+			public bool Equals(TextFileContents x, TextFileContents y)
+			{
+				if (x == null)
+					throw new ArgumentNullException("x");
+				if (y == null)
+					throw new ArgumentNullException("y");
+
+				return (
+					(x.Content == y.Content) &&
+					(x.LastModified == y.LastModified) &&
+					(x.RelativePath == y.RelativePath)
+				);
+			}
+
+			public int GetHashCode(TextFileContents obj)
+			{
+				// Doesn't matter what we return here for comparison purposes, it's fine if all instances get the same hash code, it's the
+				// Equals method that's important
+				return 0; 
+			}
+		}
+	}
+}
