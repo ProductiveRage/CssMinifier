57272112f977fdc5ab7c32da895cafb87cfd31c7
dan <742348+ProductiveRage@users.noreply.github.com>
2013-03-02 00:03:20 +0000

    Added CSS Loader factories for two basic cases which makes instantiation of the loaders much simpler

---
+++ CSSMinifier/FileLoaders/Factories/IGenerateCssLoaders.cs
@@ -0,0 +1,10 @@
+﻿namespace CSSMinifier.FileLoaders.Factories
+{
+	public interface IGenerateCssLoaders
+	{
+		/// <summary>
+		/// This will never return null, it will raise an exception if unable to satisfy the request
+		/// </summary>
+		ITextFileLoader Get();
+	}
+}
