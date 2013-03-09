3bf9ddf89fc2e8e19edf07d4069e1350cd94807b
dan <742348+ProductiveRage@users.noreply.github.com>
2013-03-09 20:04:33 +0000

    Added a couple of tests to try to prove that the last fix doesn't introduce anything nasty

--- UnitTests/LessCssLineNumberingTextFileLoaderTests.cs
+++ UnitTests/LessCssLineNumberingTextFileLoaderTests.cs
@@ -12,7 +12,47 @@ namespace UnitTests
 		{
 			var content = "body\n{\n  div.Header\n  {\n    color: black;\n  }\n}\n";
 			var expected = "#test.css_1,body\n{#test.css_3,\n  div.Header\n  {\n    color: black;\n  }\n}\n";
-			
+
+			var filename = "test.css";
+			var lineNumberInjectingContentLoader = new LessCssLineNumberingTextFileLoader(
+				new FixedListCssContentLoader(
+					new TextFileContents(filename, new DateTime(2013, 2, 5, 21, 18, 0), content)
+				),
+				(relativePath, lineNumber) => "#test.css_" + lineNumber + ","
+			);
+
+			Assert.Equal(
+				expected,
+				lineNumberInjectingContentLoader.Load(filename).Content
+			);
+		}
+
+		[Fact]
+		public void StylePropertyNamesShouldNotGetMarkers()
+		{
+			var content = "body\n{\n  color: black;\n}\n";
+			var expected = "#test.css_1,body\n{\n  color: black;\n}\n";
+
+			var filename = "test.css";
+			var lineNumberInjectingContentLoader = new LessCssLineNumberingTextFileLoader(
+				new FixedListCssContentLoader(
+					new TextFileContents(filename, new DateTime(2013, 2, 5, 21, 18, 0), content)
+				),
+				(relativePath, lineNumber) => "#test.css_" + lineNumber + ","
+			);
+
+			Assert.Equal(
+				expected,
+				lineNumberInjectingContentLoader.Load(filename).Content
+			);
+		}
+
+		[Fact]
+		public void StylePropertyNamesShouldNotGetMarkersWhenFollowedByNestedSelectorWhichShould()
+		{
+			var content = "body\n{\n  color: black;\n  div.Header { background: white; }\n}\n";
+			var expected = "#test.css_1,body\n{\n  color: black;#test.css_4,\n  div.Header { background: white; }\n}\n";
+
 			var filename = "test.css";
 			var lineNumberInjectingContentLoader = new LessCssLineNumberingTextFileLoader(
 				new FixedListCssContentLoader(
