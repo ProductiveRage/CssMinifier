bd2b3158b6ac73a3d89ff430ca98699f7d5e6611
dan <dan@newmind.co.uk>
2013-05-02 21:40:12 +0100

    Updated tests to reflect LessCssOpeningHtmlTagRenamer's replacement of LessCssOpeningBodyTagRenamer

--- UnitTests/LessCssOpeningHtmlTagRenamerTests.cs
+++ UnitTests/LessCssOpeningHtmlTagRenamerTests.cs
@@ -8,13 +8,13 @@ using UnitTests.Common;
 
 namespace UnitTests
 {
-	public class LessCssOpeningBodyTagRenamerTests
+	public class LessCssOpeningHtmlTagRenamerTests
 	{
 		[Fact]
-		public void BodyTagWithoutNestedSelectorsShouldNotBeReplaced()
+		public void HtmlTagWithoutNestedSelectorsShouldNotBeReplaced()
 		{
 			var filename = "Test.css";
-			var content = "body { }";
+			var content = "html { }";
 			var textFileLoader = new LessCssOpeningHtmlTagRenamer(
 				new FixedListCssContentLoader(
 					new TextFileContents(filename, new DateTime(2013, 1, 31, 21, 55, 0), content)
@@ -29,10 +29,10 @@ namespace UnitTests
 		}
 
 		[Fact]
-		public void BodyTagWithNestedSelectorShouldBeReplaced()
+		public void HtmlTagWithNestedSelectorShouldBeReplaced()
 		{
 			var filename = "Test.css";
-			var content = "body { h2 { font-weight: bold; } }";
+			var content = "html { h2 { font-weight: bold; } }";
 			var textFileLoader = new LessCssOpeningHtmlTagRenamer(
 				new FixedListCssContentLoader(
 					new TextFileContents(filename, new DateTime(2013, 1, 31, 21, 55, 0), content)
@@ -47,18 +47,18 @@ namespace UnitTests
 		}
 
 		/// <summary>
-		/// This test was written with the mistaken thinking that not only would the string "body" be removed but that the opening and closing braces
-		/// of the wrapping body tag be identified and potentially altered so that the tag could be removed from the final output. This is incorrect
+		/// This test was written with the mistaken thinking that not only would the string "html" be removed but that the opening and closing braces
+		/// of the wrapping html tag be identified and potentially altered so that the tag could be removed from the final output. This is incorrect
 		/// as the aim is that the replacement string need be removed from the final content but no braces as nested style declarations is only
-		/// supported by LESS and they are flattened down in the compiled CSS (so "body { div.Header { color: white; } }" becomes
-		/// "body div.Header { color: white; } }", from which only "body " need removing, not any of the braces). However this
+		/// supported by LESS and they are flattened down in the compiled CSS (so "html { div.Header { color: white; } }" becomes
+		/// "html div.Header { color: white; } }", from which only "html " need removing, not any of the braces). However this
 		/// test identified a whitespace-removal issue when added, so it's being left in.
 		/// </summary>
 		[Fact]
 		public void TrailingLineReturnsShouldBeSupported()
 		{
 			var filename = "Test.css";
-			var content = "body { h2 { font-weight: bold; } }\n";
+			var content = "html { h2 { font-weight: bold; } }\n";
 			var textFileLoader = new LessCssOpeningHtmlTagRenamer(
 				new FixedListCssContentLoader(
 					new TextFileContents(filename, new DateTime(2013, 1, 31, 21, 55, 0), content)
